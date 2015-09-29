0507194438a63415828a630c842230cc3e2f0181
Aaron Tsai <aattsai@gmail.com>
2015-09-29 15:10:25 -0400

    Complete reflection on variable-methods.rb

---
+++ week-4/variables-methods.rb
@@ -0,0 +1,27 @@
+puts "What is your first name?"
+first = gets.chomp
+puts "What is your middle name?"
+middle = gets.chomp
+puts "What is your last name?"
+last = gets.chomp
+puts "Good evening, " + first + " " + middle + " " + last
+
+puts "What is your favorite number?"
+fav = gets.chomp
+better = fav.to_i + 1
+puts better.to_s + " is a bigger and better number."
+
+/
+How do you define a local variable?
+  Give the variable a name and assign a value to it using =
+How do you define a method?
+  Use def followed by the name of the method.
+What is the difference between a local variable and a method?
+  method is like a verb that needs to act on an object. Variable is that object.
+How do you run a ruby program from the command line?
+  ruby filename.rb
+How do you run an RSpec file from the command line?
+  rspec filename.rb
+What was confusing about this material? What made sense?
+  Nothing is really confusing about this material. syntax-wise, Ruby is a relatively simple language to learn.
+/
