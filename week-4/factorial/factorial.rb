4562dd0c236bbf8a5fbd049c22ef0996634392dc
Aaron Tsai <aattsai@gmail.com>
2015-10-01 12:19:11 -0400

    Complete factorial

--- week-4/factorial/factorial.rb
+++ week-4/factorial/factorial.rb
@@ -1,13 +1,20 @@
-def factorial(number)
-fact = number
+# Factorial
 
- if number==0
-  return 1
- end
+# I worked on this challenge [by myself, with: ]. Chris Savage
+
+
+# Your Solution Below
+
+def factorial(number)
+  fact = number
+
+  if number==0
+    return 1
+  end
 
   while number > 1
-  fact = fact * (number - 1)
-  number = number - 1
+    fact = fact * (number - 1)
+    number = number - 1
   end
 
   return fact
