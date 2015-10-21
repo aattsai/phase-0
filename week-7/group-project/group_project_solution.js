8165aff5eb3e30d6496df8b9d03b0151d80058af
Aaron Tsai <aattsai@gmail.com>
2015-10-21 10:35:58 -0400

    Person 1 completion

--- week-7/group-project/group_project_solution.js
+++ week-7/group-project/group_project_solution.js
@@ -1,2 +1,24 @@
-/*As a user, I want to run the program to acquire arithmetic and statistical information based on the two arrays provided, one with an odd length, the other with an even length. More specifically this program should help the user determining the sum, the mean, and the median based on the elements in each of these two arrays.
+/*
+Group project - PERSON 1
+User story
+
+As a user, I want to run a Javascript based program that can
+perform a few arithmetic and statistical operations with integers.
+These integers will be provided in two sets of arrays, one with
+an odd number of integers, the other with an even number of integers.
+
+1. SUM - This program will have to accept all the integers from these
+two arrays, and add them up to acquire the sum of each set of integers.
+
+2. Mean - This program will take all the integers from each of these
+arrays, and find the means of each set of the numbers. Mean is
+calculated by taking the sum of the all integers, divided by the
+total number of integers.
+
+3.Median - Finally, this program will take the sets of numbers and
+calculate the median for each of the two sets. Median is defined as
+the number that lies at the midpoint of a frequency distribution. In
+other words, This program must find the number in the middle of the
+list when the numbers are sorted in order from smallest to largest.
+
 */
