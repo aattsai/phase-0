b69753da46e3551079fd818af0cb317817a75c5f
Aaron Tsai <aattsai@gmail.com>
2015-10-21 10:10:52 -0400

    Initial commit

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,2 @@
+/*As a user, I want to run the program to acquire arithmetic and statistical information based on the two arrays provided, one with an odd length, the other with an even length. More specifically this program should help the user determining the sum, the mean, and the median based on the elements in each of these two arrays.
+*/
