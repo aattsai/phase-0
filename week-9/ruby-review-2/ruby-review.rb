b30ee6bf8cac71fbbb73745a6251408a94a913c8
Aaron Tsai <aattsai@gmail.com>
2015-11-06 12:24:31 -0500

    Challenge completed

--- week-9/ruby-review-2/ruby-review.rb
+++ week-9/ruby-review-2/ruby-review.rb
@@ -28,30 +28,98 @@
 
 
 # 3. Initial Solution
+# class Car
+#   def initialize(model, color)
+#     @model = model
+#     @color = color
+#     @total_distance = 0
+#     @compartment = []
+#   end
+
+#   def drive (target_distance)
+#     @target_distance = target_distance
+#     puts "Drive #{target_distance} miles (speed limit is #{@speed_limit} mph)"
+#     @total_distance += target_distance
+#   end
+
+#   def speed_limit(number)
+#     @speed_limit = number
+#   end
+
+#   def change_speed(speed_limit)
+#     puts "You are driving at #{speed_limit} mph"
+#   end
+
+#   def check_speed
+#     puts "You are driving at #{@speed_limit} mph"
+#   end
+
+#   def turn (direction)
+#     @direction = direction
+#     if @direction == "left" || @direction == "right"
+#       puts "At the stop sign, turn #{@direction}"
+#     end
+#   end
+
+#   def pizza (type)
+#     @compartment.push(type)
+#   end
+
+#   def destination
+#     puts "Arrived! You have travelled #{@total_distance} miles. Deliever #{@compartment[0]}"
+#     @compartment.shift
+#   end
+
+# end
+
+# class Pizza
+
+#   def initialize(type, size)
+#     @type = type
+#     @size = size
+#   end
+
+#   def add_ingredient(ingredient)
+#     @ingredient = ingredient
+#   end
+
+#   def special(request)
+#     @request = request
+#   end
+
+#   def print
+#     puts "You ordered a #{@size} inch #{@type} pizza with #{@ingredient}. #{@request}"
+#   end
+
+# end
+
+
+# 4. Refactored Solution
+
 class Car
+
+  attr_reader :speed_limit
+
   def initialize(model, color)
-    @model = model
-    @color = color
     @total_distance = 0
     @compartment = []
+    puts "Get in your #{color} #{model}"
   end
 
-  def drive (target_distance)
+  def drive (target_distance, speed_limit)
+    @speed_limit = speed_limit
     @target_distance = target_distance
     puts "Drive #{target_distance} miles (speed limit is #{@speed_limit} mph)"
     @total_distance += target_distance
   end
 
-  def speed_limit(number)
-    @speed_limit = number
-  end
-
-  def change_speed(speed_limit)
-    puts "You are driving at #{speed_limit} mph"
-  end
-
-  def check_speed
-    puts "You are driving at #{@speed_limit} mph"
+  def change_speed(new_speed)
+    if new_speed == 0
+      puts "You stop the car"
+    else
+      @speed_limit = new_speed
+      puts "You are driving at #{new_speed} mph"
+    end
   end
 
   def turn (direction)
@@ -66,11 +134,12 @@ class Car
   end
 
   def destination
-    puts "Arrived! You have travelled #{@total_distance} miles. Deliever #{@compartment[0]}"
+    puts "Arrived! You have travelled #{@total_distance} miles. Deliever #{@compartment[0]} pizza"
     @compartment.shift
   end
 
 end
+
 
 class Pizza
 
@@ -90,15 +159,7 @@ class Pizza
   def print
     puts "You ordered a #{@size} inch #{@type} pizza with #{@ingredient}. #{@request}"
   end
-
 end
-
-
-# 4. Refactored Solution
-
-
-
-
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
@@ -106,16 +167,14 @@ new_car = Car.new("Infiniti", "black")
 new_car.pizza("pepperoni")
 new_car.pizza("cheese")
 new_car.pizza("meat lover")
-new_car.speed_limit(25)
-new_car.drive(0.25)
+new_car.drive(0.25, 25)
 new_car.turn("right")
-new_car.check_speed
+puts "You are driving at #{new_car.speed_limit} mph"
 new_car.change_speed(15)
-new_car.speed_limit(15)
-new_car.drive(0.25)
+new_car.drive(0.25, 15)
 new_car.turn("left")
-new_car.speed_limit(35)
-new_car.drive(1.4)
+new_car.drive(1.4, 35)
+new_car.change_speed(0)
 new_car.destination
 
 new_pizza = Pizza.new("meat lover", 8)
