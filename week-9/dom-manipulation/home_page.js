ff5e5373841e0e2c9c46b2fc39357376fefb3095
Aaron Tsai <aattsai@gmail.com>
2015-11-05 22:11:00 -0500

    Complete javascript review

--- week-9/dom-manipulation/home_page.js
+++ week-9/dom-manipulation/home_page.js
@@ -1,32 +1,41 @@
-// DOM Manipulation Challenge
-
-
-// I worked on this challenge [by myself, with: ].
-
-
-// Add your JavaScript calls to this page:
-
 // Release 0:
 
-
+document.getElementById("release-0").className = "done";
 
 
 // Release 1:
+document.getElementById("release-1").style.display = "none";
+
 
 
 // Release 2:
 
-
+document.getElementsByTagName("H1")[0].innerHTML="I completed release 2.";
 
 
 // Release 3:
+//Add the background color #955251 to the #release-3 div.
 
+document.getElementById('release-3').style.backgroundColor="#955251";
 
 
 
 // Release 4:
+var x = document.getElementsByClassName("release-4");
+var i;
+for( i = 0; i < x.length; i++ ) {
+    x[i].style.fontSize="2em";
+}
 
 
 
-
+//x = document.getElementsByClassName("example");
 // Release 5:
+//Take the HTML in the template.hidden and append it to the bottom of the page.
+
+var template = document.getElementById('hidden');
+document.body.appendChild(template.content.cloneNode(true));
+
+//var tmpl = document.getElementById('comment-template');
+//document.body.appendChild(tmpl.content.cloneNode(true));
+
--- week-9/review.js
+++ week-9/review.js
@@ -21,33 +21,74 @@
 
 
 //Initial solution
-  var list = new Object();
+  // var list = new Object();
 
-  function addList(item, quantity) {
-    list[item] = quantity;
-    console.log("You have added " + quantity +"  " + item + " to the shopping list");
-  };
+  // function addList(item, quantity) {
+  //   list[item] = quantity;
+  //   console.log("You have added " + quantity +"  " + item + " to the shopping list");
+  // };
 
-  function removeList(item) {
-    delete list[item];
-    console.log("You have deleted " + item + " from the shopping list");
-  };
+  // function removeList(item) {
+  //   delete list[item];
+  //   console.log("You have deleted " + item + " from the shopping list");
+  // };
 
-  function updateList(item, new_quant) {
-    list[item] = new_quant;
-    console.log("You have updated the quantity of " + item + " to " + new_quant);
-  };
+  // function updateList(item, new_quant) {
+  //   list[item] = new_quant;
+  //   console.log("You have updated the quantity of " + item + " to " + new_quant);
+  // };
 
-  function printList() {
-    console.log(list);
-  };
+  // function printList() {
+  //   console.log(list);
+  // };
 
-  addList("Lemonade", 2);
-  addList("Tomatoes",3);
-  addList("Onions", 1);
-  addList("Ice Cream", 4);
-  removeList("Lemonade");
-  updateList("Ice Cream", 1);
-  printList();
+  // addList("Lemonade", 2);
+  // addList("Tomatoes",3);
+  // addList("Onions", 1);
+  // addList("Ice Cream", 4);
+  // removeList("Lemonade");
+  // updateList("Ice Cream", 1);
+  // printList();
 
   //Refactored
+
+function shoppingList() {
+  this.list = {};
+
+  this.addList = function(item, quantity) {
+    this.list[item] = quantity;
+    console.log("You have added " + quantity +"  " + item + " to the shopping list");
+  };
+  this.removeList = function(item)  {
+    delete this.list[item];
+    console.log("You have deleted " + item + " from the shopping list");
+  };
+  this.updateList = function(item, quantity) {
+    this.list[item] = quantity;
+    console.log("You have updated the quantity of " + item + " to " + quantity);
+  };
+  this.printList = function() {
+    console.log(this.list);
+  };
+}
+
+var dinner = new shoppingList();
+  dinner.addList("Lemonade", 2);
+  dinner.addList("Tomatoes",3);
+  dinner.addList("Onions", 1);
+  dinner.addList("Ice Cream", 4);
+  dinner.removeList("Lemonade");
+  dinner.updateList("Ice Cream", 1);
+  dinner.printList();
+
+
+// What concepts did you solidify in working on this challenge? (reviewing the passing of information, objects, constructors, etc.)
+//   In my refactored solution, I tried creating all functions within a
+//   main function, thus making them methods. This challenge also helped
+//   me solidify the concept in using bracket notation to update a property
+//   in object.
+// What was the most difficult part of this challenge?
+//   For me, initially it was difficult to understand the role keyword "this" plays.
+// Did an array or object make more sense to use and why?
+//   I think in this example an object makes more sense to use because it can store
+//a specific quantiy for each of the items.
