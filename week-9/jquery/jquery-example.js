66e8a0cee8c3bfb81ae31de04010169ab75bd266
Aaron Tsai <aattsai@gmail.com>
2015-11-07 20:33:50 -0500

    Initial commit

--- week-9/jquery/jquery-example.js
+++ week-9/jquery/jquery-example.js
@@ -17,14 +17,27 @@ $('body').css({'background-color': 'pink'})
 
 //RELEASE 2:
   //Add code here to select elements of the DOM
-
+bodyElement = $('body')
 
 //RELEASE 3:
   // Add code here to modify the css and html of DOM elements
+$('.main').css("color", "blue");
+$('.main').css("border-bottom", "5px solid red");
+$('.main').css("visibility", "hidden");
 
+//$('.mascot').html('Copperheads');
 
 //RELEASE 4: Event Listener
   // Add the code for the event listener here
+$('img').on('mouseover', function(){
+    // e.preventDefault()
+    $(this).attr('src', 'copperhead.png')
+  })
+
+$('img').on('mouseout', function(){
+    // e.preventDefault()
+    $(this).attr('src', 'dbc_logo.png')
+  })
 
 
 //RELEASE 5: Experiment on your own
